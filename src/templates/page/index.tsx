5bd0815b8d3a9366727426ae8f63bf1f179c146c
Lucas Silva <lucassilva.uern@gmail.com>
2022-06-14 16:44:32 -0300

    início criação do template

---
+++ src/templates/page/index.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react"
+
+interface Props {
+  title: string,
+  children?: ReactElement | ReactElement[]
+}
+
+export const PageTemplate = ({ title, children }: Props) => {
+
+}
