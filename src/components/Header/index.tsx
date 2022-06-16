2fb2f1acaf40a5fa06c9f58bbea792c19f4cc6b2
Lucas Silva <lucassilva.uern@gmail.com>
2022-06-16 11:03:05 -0300

    add indicador produtos no carrinho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { cartState } from "state/atom";
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import Logo from "../Logo";
 import style from './Header.module.scss';
 
@@ -6,11 +8,28 @@ export default function Header() {
   const routes = {
     to: '/cart'
   }
+
+  const cart = useRecoilValue(cartState);
+
+  const quantity__cart = cart.map(itemCart => itemCart.quantidade_carrinho)
+    .reduce((acm, atual) => acm + atual, 0);
+
   return (
     <header className={style.header}>
       <Logo />
-      <Link to={routes.to}><i aria-label="ícone sacola de compras"
-        className={`fa-solid fa-bag-shopping ${style['purchase__icon--header']}`} ></i>
+      <Link to={routes.to}>
+        <div className={style.container__icon}>
+          <i
+            aria-label="ícone sacola de compras"
+            className={`fa-solid fa-bag-shopping 
+          ${style['purchase__icon--header']}`
+            } >
+          </i>
+          <span
+            className={quantity__cart > 0 ? style.quantity__cart : style['quantity__cart--none']}>
+            {quantity__cart}
+          </span>
+        </div>
       </Link>
     </header>
   )
