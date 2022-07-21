b0366055dda4707035fea6a3e15ee1f836a687c2
Lucas Silva <lucassilva.uern@gmail.com>
2022-07-21 13:18:31 -0300

    clear comments

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,3 @@
-// import { StyledButton } from './styles';
-
-// interface Props {
-//   onClick: () => void,
-//   children?: React.ReactNode,
-//   quantity?: number
-// }
-
-// export function Button({ onClick, children, quantity }: Props) {
-//   return (
-//     <StyledButton data-testid='test-button' disabled={quantity === 0} quantity={quantity} onClick={onClick}>
-//       {children}
-//       <i className={`fa-solid fa-bag-shopping`}></i>
-//     </StyledButton>
-//   )
-// }
-
 import { useState } from "react";
 import {
   ButtonBag,
